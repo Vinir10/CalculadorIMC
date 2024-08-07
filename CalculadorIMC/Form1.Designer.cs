a93f28bafae1a706186092c25fdba228e863b741
Vinicius Nascimento Rodrigues <viniciusrodrig2002@gmail.com>
2024-08-07 12:13:25 -0300

    Add project files.

---
+++ CalculadorIMC/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CalculadorIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.lbClassificacao = new System.Windows.Forms.Label();
+            this.lbIMC = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(28, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Peso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(25, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Altura";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(65, 65);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 2;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(65, 125);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCalcular.Location = new System.Drawing.Point(74, 171);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular IMC";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Location = new System.Drawing.Point(85, 224);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(55, 13);
+            this.lbResultado.TabIndex = 5;
+            this.lbResultado.Text = "Resultado";
+            // 
+            // lbClassificacao
+            // 
+            this.lbClassificacao.AutoSize = true;
+            this.lbClassificacao.Location = new System.Drawing.Point(80, 293);
+            this.lbClassificacao.Name = "lbClassificacao";
+            this.lbClassificacao.Size = new System.Drawing.Size(69, 13);
+            this.lbClassificacao.TabIndex = 6;
+            this.lbClassificacao.Text = "Classificação";
+            this.lbClassificacao.Visible = false;
+            // 
+            // lbIMC
+            // 
+            this.lbIMC.AutoSize = true;
+            this.lbIMC.Location = new System.Drawing.Point(97, 259);
+            this.lbIMC.Name = "lbIMC";
+            this.lbIMC.Size = new System.Drawing.Size(26, 13);
+            this.lbIMC.TabIndex = 7;
+            this.lbIMC.Text = "IMC";
+            this.lbIMC.Visible = false;
+            this.lbIMC.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(235, 368);
+            this.Controls.Add(this.lbIMC);
+            this.Controls.Add(this.lbClassificacao);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "IMC Cálculo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Label lbClassificacao;
+        private System.Windows.Forms.Label lbIMC;
+    }
+}
+
---
+++ CalculadorIMC/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadorIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double peso = Convert.ToDouble(txtPeso.Text);
+            double altura = Convert.ToDouble(txtAltura.Text);
+
+            double imc = peso / (altura * altura);
+            lbIMC.Text = imc.ToString("F2");
+            lbIMC.Visible = true;
+
+            string classificacao = string.Empty;
+
+            if (imc < 16.9)
+                classificacao = "Muito abaixo do peso";
+            else if (imc >= 17 && imc <= 18.4)
+                classificacao = "Abaixo do peso";
+            else if (imc >= 18.5 && imc <= 24.99)
+                classificacao = "Peso normal";
+            else if (imc >= 25 && imc <= 29.9)
+                classificacao = "Acima do peso";
+            else if (imc >= 30 && imc <= 34.9)
+                classificacao = "Obesidade grau I";
+            else if (imc >= 35 && imc <= 40)
+                classificacao = "Obesidade grau II";
+            else if (imc >40)
+                classificacao = "Obesidade grau III";
+
+            lbClassificacao.Text = classificacao;
+            lbClassificacao.Visible = true;
+        }
+    }
+}
